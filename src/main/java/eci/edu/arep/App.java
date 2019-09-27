a6db748a7480c027bb6df3e5f68fbe73c87958fe
unknown <2129175@labinfo.is.escuelaing.edu.co>
2019-09-27 15:51:58 -0500

    first commit

---
+++ src/main/java/eci/edu/arep/App.java
@@ -0,0 +1,9 @@
+package eci.edu.arep;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
